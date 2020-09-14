5e71f55025a8fe5c9e96196859258147bda670a9
Sergio Salvador <18466750+SergioUAH@users.noreply.github.com>
2020-09-14 20:57:51 +0200

    (WIP) Adds WebSocket support for real-time logger.

--- shodan_tfg/src/main/java/com/uah/shodan_tfg/entrypoints/MessageLoggingController.java
+++ shodan_tfg/src/main/java/com/uah/shodan_tfg/entrypoints/MessageLoggingController.java
@@ -3,25 +3,39 @@ package com.uah.shodan_tfg.entrypoints;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
 
 import com.uah.shodan_tfg.entrypoints.dto.LogMessageDTO;
 import com.uah.shodan_tfg.entrypoints.dto.LogOutput;
 
-@CrossOrigin(origins = "ws://localhost:4200")
-@RequestMapping("/log")
-@RestController
+@Controller
 public class MessageLoggingController {
 
-    @MessageMapping("/chat")
-    @SendTo("/logger/connectionLog")
-    public LogOutput send(LogMessageDTO message) throws Exception {
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    @MessageMapping("/logger")
+    @SendTo("/topic/connectionLog")
+    public LogOutput receiveMessage(@Payload LogMessageDTO message) throws Exception {
+	System.out.println("Petición recibida " + message.getText());
 	String time = "[" + new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()) + "]";
+	message.setText("Probando logs");
+	System.out.println(message.getText());
 	return new LogOutput(message.getText(), time);
+    }
+
+    public void send() {
+	LogMessageDTO message = new LogMessageDTO();
+	String time = "[" + new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()) + "]";
+	message.setText("Probando logs --> " + time);
+	System.out.println(message.getText());
+	System.out.println("Enviando mensaje --> " + message.getText());
+	this.template.convertAndSend("/topic/connectionLog", message);
     }
 
 }
