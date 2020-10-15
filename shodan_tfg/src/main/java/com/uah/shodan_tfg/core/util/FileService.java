2d778ac59c6b670a8981417a3f303411d472500b
Sergio Salvador <18466750+SergioUAH@users.noreply.github.com>
2020-10-15 19:41:52 +0200

    Fixes file import.

--- shodan_tfg/src/main/java/com/uah/shodan_tfg/core/util/FileService.java
+++ shodan_tfg/src/main/java/com/uah/shodan_tfg/core/util/FileService.java
@@ -1,7 +1,10 @@
 package com.uah.shodan_tfg.core.util;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import org.jboss.logging.Logger;
 import org.springframework.stereotype.Service;
@@ -10,18 +13,38 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class FileService implements IFileService {
 
-    private static Logger LOGGER = Logger.getLogger(FileService.class);
+	private static Logger LOGGER = Logger.getLogger(FileService.class);
 
-    @Override
-    public void saveFile(MultipartFile file) {
-	File destination = new File("/wordlist", file.getName());
-	try {
-	    file.transferTo(destination);
-	} catch (IllegalStateException e) {
-	    LOGGER.error(e.getMessage(), e);
-	} catch (IOException e) {
-	    LOGGER.error(e.getMessage(), e);
+	@Override
+	public void saveFile(MultipartFile file) {
+		InputStream inputStream = null;
+		OutputStream outputStream = null;
+		String rootPath = System.getProperty("user.dir");
+		File dir = new File(rootPath + File.separator + "src" + File.separator
+				+ "main" + File.separator + "resources" + File.separator
+				+ "wordlists");
+		if (!dir.exists())
+			dir.mkdirs();
+		File destination = new File(
+				dir.getAbsolutePath() + File.separator
+						+ file.getOriginalFilename());
+		try {
+			if (!destination.exists()) {
+				destination.createNewFile();
+			}
+			inputStream = file.getInputStream();
+			outputStream = new FileOutputStream(destination);
+			Integer read = 0;
+			byte[] bytes = new byte[1024];
+
+			while ((read = inputStream.read(bytes)) != -1) {
+				outputStream.write(bytes, 0, read);
+			}
+		} catch (IllegalStateException e) {
+			LOGGER.error(e.getMessage(), e);
+		} catch (IOException e) {
+			LOGGER.error(e.getMessage(), e);
+		}
 	}
-    }
 
 }
